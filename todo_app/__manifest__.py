347d31ed5566318288e02a1a5fbdfafb39863fef
Olalekan Babawale <padinality@yahoo.com>
2025-03-31 17:17:21 +0100

    [ADD] todo_app: add todo application

---
+++ todo_app/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Todo App",
+    "version": "1.0",
+    "category": "Productivity",
+    "summary": "A simple Todo application",
+    "description": """
+    A simple Todo application built with Odoo.
+    """,
+    "author": "Olalekan Babawale",
+    "website": "https://obabawale.github.io",
+    "license": "LGPL-3",
+}
